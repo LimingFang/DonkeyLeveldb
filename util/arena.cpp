ef9be85cf5a28a4047a6318a7119c1df8e3d7416
limingfang <limingfang97@outlook.com>
2022-03-05 13:25:58 +0800

    实现 Arena

--- util/arena.cpp
+++ util/arena.cpp
@@ -3,3 +3,51 @@
 //
 
 #include "arena.h"
+
+#include <cassert>
+
+namespace leveldb {
+const int kBlockSize = 4 << 10;
+
+Arena::Arena() : blocks_({}), ptr_(nullptr), avail_(0) {}
+
+Arena::~Arena() {
+  for (auto p : blocks_) {
+    delete[] p;
+  }
+}
+
+char* Arena::Allocate(size_t bytes) {
+  assert(bytes > 0);
+  if (bytes <= avail_) {
+    char* result = ptr_;
+    ptr_ += bytes;
+    avail_ -= bytes;
+    return result;
+  }
+  return AllocateFallback(bytes);
+}
+
+char* Arena::AllocateFallback(size_t bytes) {
+  if (bytes >= kBlockSize / 4) {
+    return AllocateNewBlock(bytes);
+  }
+  ptr_ = AllocateNewBlock(kBlockSize);
+  avail_ = kBlockSize;
+  char* result = ptr_;
+  ptr_ += bytes;
+  avail_ -= bytes;
+
+  return result;
+}
+
+char* Arena::AllocateNewBlock(size_t block_bytes) {
+  char* ptr = new char[block_bytes];
+  assert(ptr);
+  blocks_.push_back(ptr);
+  memory_usage_.fetch_add(block_bytes + sizeof(char*),
+                          std::memory_order_relaxed);
+
+  return ptr;
+}
+}  // namespace leveldb
--- util/arena.h
+++ util/arena.h
@@ -5,6 +5,7 @@
 #ifndef DONKEYLEVELDB_UTIL_ARENA_H_
 #define DONKEYLEVELDB_UTIL_ARENA_H_
 #include <cstddef>
+#include <vector>
 
 namespace leveldb {
 class Arena {
@@ -16,6 +17,16 @@ class Arena {
   Arena& operator=(const Arena&) = delete;
 
   char* Allocate(size_t bytes);
+
+ private:
+  char* AllocateFallback(size_t bytes);
+
+  char* AllocateNewBlock(size_t block_bytes);
+
+  std::vector<char*> blocks_;
+  char* ptr_;
+  int avail_;
+  std::atomic<size_t> memory_usage_;
 };
 }  // namespace leveldb
 
